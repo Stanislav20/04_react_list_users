5bba65a6adfcb039109f030d1ac88eafa21e443a
Stanislav20 <stas_on38@mail.ru>
2023-03-19 21:04:38 +0800

    list-users

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Success from './components/Success';
+import Users from './components/Users';
+
+function App() {
+  return (
+    <div className="App">
+      <Users />
+      {/* <Success /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Success.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Success.css'
+
+function Success() {
+  return (
+    <div class="success-block">
+      <img src="/assets/success.svg" alt="Success" />
+      <h3>Успешно!</h3>
+      <p>Всем пользователям отправлено приглашение.</p>
+      <button className="send-invite-btn">Назад</button>
+    </div>
+  );
+};
+
+export default Success
