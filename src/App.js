71ab27aa2a7122f115ad73dbe854de47921e0be3
Stanislav20 <stas_on38@mail.ru>
2023-03-20 20:56:00 +0800

    Добавлена логика получения и рендера данных пользователей, "скелетон" на время получения данных пользователей, получение значения введенного в строке поиска.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import Success from './components/Success';
 import Users from './components/Users';
 
 function App() {
+	const [items, setItems] = useState([])
+	const [isLoading, setIsLoading] = useState(true)
+	const [searchValue, setSearchValue] = useState('')
+
+	useEffect(() => {
+		fetch('https://reqres.in/api/users')
+		.then((res) => res.json())
+		.then((json) => setItems(json.data))
+		.catch((error) => console.log(error.message))
+		.finally(() => setIsLoading(false))
+	}, [])
+
+	const onChangeSearchValue = (event) => {
+		setSearchValue(event.target.value)
+		console.log(event.target.value)
+	}
+
   return (
     <div className="App">
-      <Users />
+      <Users
+				items={items} 
+				isLoading={isLoading} 
+				searchValue={searchValue} 
+				onChangeSearchValue={onChangeSearchValue} />
       {/* <Success /> */}
     </div>
   );
